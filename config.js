1aa36f7e79244339f0810fafb9ace6da918db44a
dullar255 <dullahbest04@gmail.com>
2025-04-14 16:14:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923143702270",
+DEV: process.env.DEV || "255716945971",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
