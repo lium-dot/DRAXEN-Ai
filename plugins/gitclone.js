e4cfe25b1cc62fc1572f588a132514d441aee9a6
dullar255 <dullahbest04@gmail.com>
2025-04-14 11:44:50 +0200

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -43,7 +43,7 @@ cmd({
     const fileName = contentDisposition ? contentDisposition.match(/filename=(.*)/)[1] : `${repo}.zip`;
 
     // Notify user of the download
-    reply(`📥 *Downloading repository...*\n\n*Repository:* ${username}/${repo}\n*Filename:* ${fileName}\n\n> *© Pᴏᴡᴇʀᴇᴅ Bʏ 𓄂𝕚𝕥𝕩.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼ ♡*`);
+    reply(`📥 *Downloading repository...*\n\n*Repository:* ${username}/${repo}\n*Filename:* ${fileName}\n\n> *© Pᴏᴡᴇʀᴇᴅ Bʏ DRAXEN-Ai 🤎*`);
 
     // Send the zip file to the user with custom contextInfo
     await conn.sendMessage(from, {
@@ -55,8 +55,8 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363333032882285@newsletter',
-          newsletterName: '𓄂𝕚𝕥𝕩.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼ ♡',
+          newsletterJid: '0029Vb4vbKF6xCSMEFopxc10@newsletter',
+          newsletterName: 'DRAXEN Ai 🤎',
           serverMessageId: 143
         }
       }
