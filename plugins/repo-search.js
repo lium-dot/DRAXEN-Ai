f139223897063f06763bf4857d836b00f4126394
dullar255 <dullahbest04@gmail.com>
2025-04-14 13:48:14 +0200

    Update repo-search.js

--- plugins/repo-search.js
+++ plugins/repo-search.js
@@ -25,7 +25,7 @@ cmd({
     responseMsg += `🍴 *Forks*: ${data.forks_count}\n`;
     responseMsg += `👤 *Owner*: ${data.owner.login}\n`;
     responseMsg += `📅 *Created At*: ${new Date(data.created_at).toLocaleDateString()}\n`;
-    responseMsg += `\n> *© Pᴏᴡᴇʀᴇᴅ Bʏ 𓄂𝕚𝕥𝕩.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼ ♡*`;
+    responseMsg += `\n> *© Pᴏᴡᴇʀᴇᴅ Bʏ DRAXEN Ai 🤎*`;
 
     await conn.sendMessage(from, { text: responseMsg }, { quoted: m });
   } catch (error) {
